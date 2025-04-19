671aeadee37050acbcbbcdf3648b462ee170b27c
MayaraMB <maybastos2021@gmail.com>
2025-04-19 18:46:08 -0300

    feat: menu de criacao de conta e escolha de operacao

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,69 @@
+import java.util.Scanner;
+
 
 public class Main {
 
 	public static void main(String[] args) {
-		Cliente venilton = new Cliente();
-		venilton.setNome("Venilton");
-		
-		Conta cc = new ContaCorrente(venilton);
-		Conta poupanca = new ContaPoupanca(venilton);
+		Scanner sc = new Scanner(System.in);
+		Cliente cliente = new Cliente();
 
-		cc.depositar(100);
-		cc.transferir(100, poupanca);
+		// implementar novo cliente
+		System.out.println("Digite o nome do cliente: ");
+		cliente.setNome(sc.next());
+
+		// implementar nova conta para o cliente
+		Conta cc = new ContaCorrente(cliente);
+		Conta poupanca = new ContaPoupanca(cliente);
+		Conta contaGenerica = null;
+
+System.out.println("Em qual tipo de conta deseja fazer a operação em conta Corrente?: Y/N");
+		String tipoConta = sc.next();
+		if (tipoConta.equalsIgnoreCase("Y")) {
+			System.out.println("Você escolheu conta Corrente.");
+			 contaGenerica = cc;
+		} else if (tipoConta.equalsIgnoreCase("N")) {
+			System.out.println("Você escolheu conta Poupança.");
+			contaGenerica = poupanca;
+		} else {
+			System.out.println("Opção inválida. Por favor, escolha Y ou N.");
+		}
+
+
 		
-		cc.imprimirExtrato();
+	
+
+		//implementar escolha de operacao
+		System.out.println("Escolha a operação: ");
+		System.out.println("1 - Depositar");
+		System.out.println("2 - Sacar");
+		System.out.println("3 - Transferir");
+		System.out.println("4 - Extrato");
+		int operacao = sc.nextInt();
+
+		switch (operacao) {
+			case 1:
+				System.out.println("Digite o valor a ser depositado: ");
+				double valorDeposito = sc.nextDouble();
+				contaGenerica.depositar(valorDeposito);
+				break;
+			case 2:
+				System.out.println("Digite o valor a ser sacado: ");
+				double valorSaque = sc.nextDouble();
+				contaGenerica.sacar(valorSaque);
+				break;
+				case 3:
+				System.out.println("Digite o valor a ser transferido: ");
+				double valorTransferencia = sc.nextDouble();
+				contaGenerica.transferir(valorTransferencia, poupanca);
+				break;
+			case 4:
+			contaGenerica.imprimirExtrato();
+				break;
+			default:
+				System.out.println("Operação inválida!");
+		}
+
+
 		poupanca.imprimirExtrato();
 	}
 
